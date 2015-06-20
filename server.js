1c4decebb34fd922408dadd8b413b232aa2e2094
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-06-20 16:44:43 -0700

    Fix express

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var app = require('express');
+var express = require('express');
 var restbus = require('restbus');
 var http = require('http');
 var cors = require('cors');
 
+var app = express();
 app.use(cors());
  
-http.createServer(app).listen('3030', function() {
+http.createServer(express).listen('3030', function() {
   console.log('app is now listening on port 3030');
   restbus.listen(function() {
     console.log('restbus is now listening on port 3535');
